08f2892e690a29a736bcb0e5efb7ea69abf1b035
Alessandro Alvarado <aav6554@gmail.com>
2025-04-08 23:40:23 -0500

    Add files via upload

---
+++ Results_Class.java
@@ -0,0 +1,35 @@
+// ******************************************************************** 
+// Results_Class.java       Author: Alessandro Alvarado
+//
+// Shows the results when you choose to run files. This program displays 
+// detailed information about different animals, including their species, 
+// behavior (e.g., social or solitary), and specific characteristics 
+// (e.g., gestation time, jaw strength, diet type). It also calculates 
+// and displays the current habitat size for each animal. The results 
+// include animals like birds (e.g., Northern Cardinal, Galapagos Penguin) 
+// and mammals (e.g., African Bush Elephant, Black Bear, West African Lion, 
+// Ruffed Lemur), showcasing their unique attributes and their space requirements.
+// ********************************************************************
+
+
+Animal name Cardinal-1, is one of the species Northern Cardinal. It is a social animal. This bird can fly! 
+Current habitat size: 100.0 square feet.
+-----------------------------------
+It is a social animal. Animal name Elephant-1, is one of the species African Bush Elephant. This mammal's gestation time is 650.0 days. This herbivore is a type of folivore. 
+Current habitat size: 400.0 square feet.
+-----------------------------------
+It is solitary. Animal name Bear-1, is one of the species Black Bear. This mammal's gestation time is 220.0 days. 
+Current habitat size: 650.0 square feet.
+-----------------------------------
+It is a social animal. Animal name Lion-1, is one of the species West African Lion. This mammal's gestation time is 115.5 days. The jaw strength of this carnivore is 650.0 psi. It lives in a family unit.
+Current habitat size: 650.0 square feet.
+-----------------------------------
+It is a social animal. Animal name Lemur-1, is one of the species Ruffed Lemur. This mammal's gestation time is 102.0 days. This herbivore is a type of frugivore. 
+Current habitat size: 400.0 square feet.
+-----------------------------------
+It is solitary. Animal name Bear-2, is one of the species Kodiak Bear. This mammal's gestation time is 230.0 days. 
+Current habitat size: 650.0 square feet.
+-----------------------------------
+Animal name Penguin-1, is one of the species Galapagos Penguin. It is a social animal. This bird does not fly. 
+Current habitat size: 100.0 square feet.
+-----------------------------------
